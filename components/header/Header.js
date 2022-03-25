6558b55e43a3a0eaee84dcc7b9752b72f706a71f
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-03-25 20:46:53 +0100

    header re-fix

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,26 +1,31 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Image from "next/image";
+import logo from "../../public/img/MAIN LOGO.png"
 
 const Nav = styled.nav`
   padding: 0 20px;
   min-height: 9vh;
   display: flex;
+  width: 80%;
+  margin: auto;
   justify-content: space-between;
   align-items: center;
+
+  @media (max-width:500px){
+    width: 100%;
+  }
 `;
 
-const Logo = styled.h1`
+const Logo = styled.div`
   font-size: 25px; 
-  color: red;
+  display: flex;
+  align-items: center;
 `;
 
 const Menu = styled.ul`
   list-style: none;
   display: flex;
-
-  li:nth-child(2) {
-    margin: 0px 20px;
-  }
 
   @media (max-width: 768px) {
     display: none;
@@ -30,8 +35,10 @@ const Menu = styled.ul`
 const Item = styled.li``;
 
 const Link = styled.a`
-  color: red;
   text-decoration: none;
+  color: gray;
+  padding-left: 20px;
+ 
 
   :hover {
     text-decoration: underline;
@@ -65,7 +72,7 @@ const Line = styled.span`
 
 const Overlay = styled.div`
   position: absolute;
-  height: ${props => (props.open ? "91vh" : 0)};
+  height: ${props => (props.open ? "50vh" : 0)};
   width: 100vw;
   background-color: black;
   transition: height 0.4s ease-in-out;
@@ -99,7 +106,15 @@ const Header = () => {
   return (
     <>
       <Nav>
-        <Logo>CSS Tricks</Logo>
+        <Logo>
+             <Image
+               src={logo}
+               alt={"logo"}
+               width={40}
+               height={40}
+             />
+             <h4>College</h4>
+        </Logo>
         <Menu>
           <Item>
             <Link target="#" href="https://www.instagram.com/igor_dumencic/">
@@ -108,12 +123,17 @@ const Header = () => {
           </Item>
           <Item>
             <Link target="#" href="https://www.behance.net/igordumencic">
-               Why us?
+                why us?
             </Link>
           </Item>
           <Item>
             <Link target="#" href="https://github.com/Igor178">
-              Github
+              About us
+            </Link>
+          </Item>
+          <Item>
+            <Link target="#" href="https://github.com/Igor178">
+              Contact us
             </Link>
           </Item>
         </Menu>
@@ -125,19 +145,24 @@ const Header = () => {
       </Nav>
       <Overlay open={toggle}>
         <OverlayMenu open={toggle}>
-          <Item>
+        <Item>
             <Link target="#" href="https://www.instagram.com/igor_dumencic/">
-              Instagram
+              Home
             </Link>
           </Item>
           <Item>
             <Link target="#" href="https://www.behance.net/igordumencic">
-              Behance
+                why us?
             </Link>
           </Item>
           <Item>
             <Link target="#" href="https://github.com/Igor178">
-              Github
+              About us
+            </Link>
+          </Item>
+          <Item>
+            <Link target="#" href="https://github.com/Igor178">
+              Contact us
             </Link>
           </Item>
         </OverlayMenu>
