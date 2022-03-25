b40f7a5a4d48ed0b163f6955c437c050236d0f50
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-03-25 21:50:14 +0100

    hero and css wave

--- layout/Default.js
+++ layout/Default.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import Header from '../components/header/Header'
+import Hero from '../components/hero/Hero'
+import styled from "styled-components"
+
+const BgImg=styled.div`
+   background: red;
+   width: 100%;
+   height: auto;
+
+`
+
 export default function Default({children}) {
   return (
     <div>
-        <Header/>
+        <BgImg>
+          <Header/>
+          <Hero/>
+        </BgImg>
         <main>
-        {  children}
+           {children}
         </main>
     </div>
   )
